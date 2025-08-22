7affebdedf50f6d85d141855c66760b82c5e52f5
Emiliano Cassoni <cassoniemiliano@gmail.com>
2025-08-22 13:43:46 -0300

    [feat] userSchema

---
+++ src/schema/userSchema.js
@@ -0,0 +1,29 @@
+import mongoose, { model } from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 20,
+    },
+    surname: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const userModal = model('User', userSchema);
+
+export default userModal;
